b70ca9942e648385e29bf76914a4efa3afd4090b
Addis <pupiatpixar@gmail.com>
2023-07-20 14:08:39 +0300

    Javascript session : added

---
+++ 1x00.JavaScript/README.md
@@ -0,0 +1,9 @@
+# JavaScript
+An introduction to javascript starting projects and cheat sheets
+* What is JavaScript
+* Why JavaScript
+* Variables & Data Types
+* Arrays
+* Loops (for, while, do while, for . . . of, forEach & map)
+* Conditionals (if, ternary & switch)
+* Functions
---
+++ 1x00.JavaScript/assets/js/script.js
@@ -0,0 +1,48 @@
+console.log("Hello world");
+
+console.group("First Part");
+console.log(25);
+console.warn("Warning Message");
+console.table({name: 'Abebe', age: '25', address : 'Bole' })
+console.groupEnd();
+
+
+console.log("Hello", "THello is Me")
+style = "background: red";
+
+console.log('%cHello World', style);
+
+
+let firstName = "John";
+let fatherName = "Doe";
+
+let x;
+x = 7, y =3;
+let sum = 7 +3;  // 10
+x = sum + y; // 13
+
+const VAL = 3.22;
+
+console.log(VAL);
+
+let sample = null;
+
+id = Symbol('id');
+
+let flag = true;
+
+let largeNum =  900719254740991n;
+
+let num = [1,2,3,4];
+
+function greetings(){
+   console.log("Hi everyone");
+}
+
+product = {
+    name : 'Burger',
+    ingredients: ['sauce', 'onion', 'egg', 'cheese'],
+    price : 25
+}
+
+console.log(product, typeof product);
